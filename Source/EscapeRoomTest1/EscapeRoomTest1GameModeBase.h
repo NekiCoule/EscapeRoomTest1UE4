6b857dc211417db794ee7729e23897343459af0b
Unknown <webermax.mail@gmail.com>
2019-02-05 21:42:09 +0100

    First push

---
+++ Source/EscapeRoomTest1/EscapeRoomTest1GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeRoomTest1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOMTEST1_API AEscapeRoomTest1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
